74cfd8945080d7c6b4ed41e0e044d34528121f13
Andres Molano Lujan <andresmolano12@gmail.com>
2022-05-09 17:44:01 -0500

    change credentials infra1

--- infra/config.tf
+++ infra/config.tf
@@ -1,9 +1,13 @@
-variable "region" {
-  default     = "us-east-1"
-  description = "AWS region"
+provider "aws" {
+  profile = "default"
+  region  = "us-east-1"
 }
 
-provider "aws" {
-  region = var.region
-  profile = "amolanol"  
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.11.0"
+    }
+  }
 }
